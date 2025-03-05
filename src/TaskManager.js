0864c41f669cfc7c9375210352f1070354033b5e
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-03-05 21:04:41 +0530

    Added a unique id attribute to each todo

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -1,6 +1,4 @@
 // TaskManager.js
-
-
 import ProjectManager from "./ProjectManager";
 
 export default class TaskManager {
@@ -11,13 +9,18 @@ export default class TaskManager {
   addTask(title, dueDate, priority, projectName = "default") {
     let isComplete = false;
     const projectList = this.projectManager.getProject(projectName);
-    projectList.push({ title, dueDate, priority, isComplete });
+    const id = projectList.length;
+    projectList.push({ id, title, dueDate, priority, isComplete });
   }
 
   getTask(projectName = "default") {
     for (const todos of this.projectManager.getProject(projectName))
       console.log(todos);
   }
+
+  // changePriority(priority){
+
+  // }
 
   test() {
     this.projectManager.display();
